143f46c4429c8392269e3b923cb5558431340ece
charbeltabet <charbeltabet@live.com>
2021-11-02 18:58:27 -0400

    Minor fixes

--- src/routes/redirects.js
+++ src/routes/redirects.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Url = require('../model/urlModel');
-const {increaseCounter} = require('../controller/url-redirection')
 // : app.get(/:code)
 
 // @route       GET /:code
